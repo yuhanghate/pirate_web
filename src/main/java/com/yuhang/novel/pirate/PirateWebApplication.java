a45ecac8edeaf21d6b4b6271e1ce622483c02e7a
yuhang <714610354@qq.com>
2019-08-04 00:10:16 +0800

    配环境

--- src/main/java/com/yuhang/novel/pirate/PirateWebApplication.java
+++ src/main/java/com/yuhang/novel/pirate/PirateWebApplication.java
@@ -1,9 +1,11 @@
 package com.yuhang.novel.pirate;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yuhang.novel.pirate.mapper")
 public class PirateWebApplication {
 
     public static void main(String[] args) {
