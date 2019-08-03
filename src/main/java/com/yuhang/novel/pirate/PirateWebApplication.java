152b88ca09e659c10b62cd359b127c8bb90ac993
yuhang <714610354@qq.com>
2019-08-03 23:48:11 +0800

    crate project

---
+++ src/main/java/com/yuhang/novel/pirate/PirateWebApplication.java
@@ -0,0 +1,13 @@
+package com.yuhang.novel.pirate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PirateWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PirateWebApplication.class, args);
+    }
+
+}
